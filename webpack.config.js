4e93882e9a5523942169112bf9d17a14beb372b5
gaheecho <gahee.cho@traport.com>
2021-01-27 09:26:33 +0900

    Add files via upload
    webpack.config파일 빠진 것 추가

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'none',
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    hot: true,
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
